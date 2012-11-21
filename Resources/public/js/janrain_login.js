5f81fb23464945ebdcdf5223a97364bc1ae74c57
Alexander Murashov <rodger@ladela.com>
2012-11-21 14:27:42 +0200

    use janrain application name in the loading script tag

--- Resources/public/js/janrain_login.js
+++ Resources/public/js/janrain_login.js
@@ -1,4 +1,12 @@
-(function () {
+scripts = document.getElementsByTagName( 'script' );
+thisScriptTag = scripts[ scripts.length - 1 ];
+
+(function ($, thisScriptTag) {
+  var $script = $(thisScriptTag);
+  try {
+  if ($script.attr('application_name') === undefined) console.error('Janrain loader script must have application_id parameter');
+  } catch(err) { return false;}
+
   if (typeof window.janrain !== 'object') window.janrain = {};
   if (typeof window.janrain.settings !== 'object') window.janrain.settings = {};
 
@@ -9,9 +17,8 @@
 
   function isReady() {
     janrain.ready = true;
-  }
+  };
 
-  ;
   if (document.addEventListener) {
     document.addEventListener("DOMContentLoaded", isReady, false);
   } else {
@@ -23,11 +30,11 @@
   e.id = 'janrainAuthWidget';
 
   if (document.location.protocol === 'https:') {
-    e.src = 'https://rpxnow.com/js/lib/usetwice-local/engage.js';
+    e.src = 'https://rpxnow.com/js/lib/' + $script.attr('application_name') + '/engage.js';
   } else {
-    e.src = 'http://widget-cdn.rpxnow.com/js/lib/usetwice-local/engage.js';
+    e.src = 'http://widget-cdn.rpxnow.com/js/lib/' + $script.attr('application_name') + '/engage.js';
   }
 
   var s = document.getElementsByTagName('script')[0];
   s.parentNode.insertBefore(e, s);
-})();
+})(jQuery, thisScriptTag);
